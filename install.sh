7e3d8cbfecbe0db985362c744c09384b3dfa88dc
CaueAlvim <caue.eyti@gmail.com>
2024-02-18 19:25:14 -0300

    added install script / changed directory structure / added wallpapers

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Update system
+pacman -Syu
+
+# Making .config and main folders
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/Documentos
+mkdir -p /home/$username/Downloads
+mkdir -p /home/$username/Imagens
+mkdir -p /home/$username/Projetos
+mkdir -p /home/$username/Músicas
+
+# Making backup of bashrc
+mv .bashrc .bashrcBak
+
+# Moving resources
+cd /home/$username/hyprlandDots/
+mv .bashrc /home/$username/
+mv .config/ /home/$username/
+mv Wallpapers/ /home/$username/Imagens/
+
+# Ensuring that the user with UID 1000 owns all files and directories within their home directory. 
+# chown -R $username:$username /home/$username
+
+# Install package list
+pacman -Sy hyprland kitty xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit polkit-gnome ufw
+
+# Installing Essential Programs 
+pacman -Sy waybar swaybg dunst nautilus file-roller htop nano starship ttf-inconsolata-nerd
+
+# Installing Firefox
+pacman -Sy firefox
+
+# Config ufw
+ufw default deny
+ufw enable
+systemctl enable ufw
+
